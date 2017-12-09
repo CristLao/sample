fffdaf8b59474a65f5f95c35a554b57a78b9fec5
CristLoader <working_lao@163.com>
2017-12-09 22:52:11 +0800

    完成静态页面修改

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-cn">
 <head>
     <meta charset="UTF-8">
-    <title>Sample App</title>
+    <title>@yield('title', 'Sample App') - Laravel入门</title>
 </head>
 <body>
     @yield('content')
